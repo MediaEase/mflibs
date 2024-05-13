820a54012fa1a79766019e15f170755f78d4f0a8
tomcdj71 <contact.tomc@yahoo.fr>
2024-05-13 10:08:31 +0200

    feat: add 'mflibs::distro::report' function

--- src/libs/distro.sh
+++ src/libs/distro.sh
@@ -6,11 +6,11 @@
 # @project_name: mflibs
 # @brief: a library for distro related functions
 #
-# @author: Jamie Dobbs (mschf)
-# @author_contact: jamie.dobbs@mschf.dev
+# @author: Jamie Dobbs (mschf), Thomas Chauveau (tomcdj71)
+# @author_contact: jamie.dobbs@mschf.dev, contact.tomc@yahoo.fr
 #
 # @license: BSD-3 Clause (Included in LICENSE)
-# Copyright (C) 2021-2022, Jamie Dobbs
+# Copyright (C) 2021-2024, Jamie Dobbs, Thomas Chauveau
 # All rights reserved.
 ################################################################################
 
@@ -59,7 +59,7 @@ mflibs::distro::version() {
       source /etc/lsb-release
       distro_version="${DISTRIB_RELEASE}"
     else
-    [[ " ${MFLIBS_LOADED[*]} " =~ verbose ]] && echo -ne "[$(tput setaf 1)1$(tput sgr0)]: unable to detect distro version\n" >&2
+      [[ " ${MFLIBS_LOADED[*]} " =~ verbose ]] && echo -ne "[$(tput setaf 1)1$(tput sgr0)]: unable to detect distro version\n" >&2
       return 1
     fi
     printf -- "%s" "${distro_version}"
@@ -71,39 +71,50 @@ mflibs::distro::version() {
 # @example:
 #   mflibs::distro::name
 # @return_code: 0 success
-# @return_code: 1 unable to detect
+# @return_code: 1 unable to detect distro codename
+# @return_code: 2 unable to detect distro version
+# @return_code: 3 unable to identify system information from /etc/os-release
 ################################################################################
 mflibs::distro::report() {
-  declare -g os_name os_version packagetype
-  if [[ -f /etc/os-release ]]; then
-    source "/etc/os-release"
-    os_name="${ID}"
-    os_version="${VERSION_ID}"
-    packagetype="Unknown"
+    declare -g os_name os_version os_codename packagetype
+    # shellcheck disable=SC2034
+    os_codename=$(mflibs::distro::codename) || {
+      printf -- "Unable to detect distro codename.\n"
+      return 1
+    }
+    os_version=$(mflibs::distro::version) || {
+      echo "Unable to detect distro version."
+      printf -- "Unable to detect distro version.\n"
+      return 2
+    }
+    if [[ -f /etc/os-release ]]; then
+      source "/etc/os-release"
+      os_name="${ID}"
+      packagetype="Unknown"
 
-    case "${ID}" in
-      ubuntu|debian|raspbian)
-        packagetype="apt"
-        ;;
-      centos|rhel|fedora|amzn)
-        packagetype="dnf"
-        [[ "${VERSION_ID%%.*}" = "7" ]] && packagetype="yum"
-        ;;
-      arch|manjaro)
-        packagetype="pacman"
-        ;;
-      opensuse*|sles)
-        packagetype="zypper"
-        ;;
-      alpine)
-        packagetype="apk"
-        ;;
-      *)
-        packagetype="Not supported"
-        ;;
-    esac
-  else
-    echo "Unable to identify system information."
-    return 1
-  fi
+      case "${ID}" in
+        ubuntu|debian|raspbian)
+          packagetype="apt"
+          ;;
+        centos|rhel|fedora|amzn)
+          packagetype="dnf"
+          [[ "${VERSION_ID%%.*}" = "7" ]] && packagetype="yum"
+          ;;
+        arch|manjaro)
+          packagetype="pacman"
+          ;;
+        opensuse*|sles)
+          packagetype="zypper"
+          ;;
+        alpine)
+          packagetype="apk"
+          ;;
+        *)
+          packagetype="Not supported"
+          ;;
+      esac
+    else
+      printf -- "Unable to identify system information from /etc/os-release.\n"
+      return 3
+    fi
 }
